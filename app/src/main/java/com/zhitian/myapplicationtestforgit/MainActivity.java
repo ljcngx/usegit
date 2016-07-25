881c430a6291d6c5c764f8fb9cb9f3005861faa4
lijun <1255191609@qq.com>
2016-07-25 16:58:58 +0800

    第三次测试

---
+++ app/src/main/java/com/zhitian/myapplicationtestforgit/MainActivity.java
@@ -0,0 +1,26 @@
+package com.zhitian.myapplicationtestforgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("朋友你好啊！");
+        System.out.println("好啊");
+
+
+        System.out.println("how old are you");
+        System.out.println("how old are you");
+        System.out.println("how old are you");
+        System.out.println("how old are you");
+
+
+
+
+    }
+}
